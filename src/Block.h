5e0ec1834d5d18961f8f73dc311cf79ce7dc4ef7
chrismphilp <chrismphilp@hotmail.co.uk>
2018-05-04 19:18:49 +0100

    Working bricks in GUI

--- src/Block.h
+++ src/Block.h
@@ -3,16 +3,26 @@
 class Block {
 	public:
 		int health;
-		int height;
 		int width;
+		int height;
 		float x_origin;
 		float y_origin;
 
-		Block(int height, float width, float x_origin, float y_origin, int health) :
-			height(height),
+		Block() {};
+
+		Block(int width, int height, float x_origin, float y_origin, int health) :
 			width(width),
+			height(height),
 			x_origin(x_origin),
 			y_origin(y_origin),
 			health(health)
 		{}
+
+		void setValues(int width, int height, float x_origin, float y_origin, int health) {
+			Block::width = width;
+			Block::height = height;
+			Block::x_origin = x_origin;
+			Block::y_origin = y_origin;
+			Block::health = health;
+		}
 };
--- src/BlockMatrix.h
+++ src/BlockMatrix.h
@@ -4,20 +4,30 @@
 
 class BlockMatrix {
 	public:
-		Block* arrayBlock;
-		int x_rows;
-		int y_columns;
+		Block blockArray[10][10];
+		float block_width;
+		float block_height;
+		int x_matrix_origin;
+		int y_matrix_origin;
+		int x_matrix_max;
+		int y_matrix_max;
 		int block_probability; // 1-100
 
-		BlockMatrix(int x_rows, int y_columns, int block_probability) :
-			blockArray(new Block[x_rows][y_columns]),
+		BlockMatrix(float block_width, float block_height, int x_matrix_origin, int y_matrix_origin, int x_matrix_max, 
+			int y_matrix_max, int block_probability) :
+			block_width(block_width),
+			block_height(block_height),
+			x_matrix_origin(x_matrix_origin),
+			y_matrix_origin(y_matrix_origin),
+			x_matrix_max(x_matrix_max),
+			y_matrix_max(y_matrix_max),
 			block_probability(block_probability)
 		{}
 
-		initiliaseBricks() {
-			for (int x = 0; x < x_rows; x++) {
-				for (int y = 0; y < y_columns; y++) {
-					blockArray[x][y] = new Block(1, 10, 20);
+		void initiliaseBricks(void) {
+			for (int x = 0; x < 10; x++) {
+				for (int y = 0; y < 10; y++) {
+					blockArray[x][y].setValues(block_width, block_height, x_matrix_origin+(x*55), y_matrix_origin+(y*15), 1);
 				}
 			}
 		}
